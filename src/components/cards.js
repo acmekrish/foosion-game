4d8e004b60147f429ec25c15119801ddbf0f3c71
Krish Acme <acmekrish@gmail.com>
2022-08-18 14:13:21 +0100

    alpha

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import Card from "./card";
+import { useState } from 'react'
+import Card from './card'
 
-const Cards = () => {
-    const [items, setItems] = useState ([
-        {id: 1, img: '/img/1.png', stat: ""},
-        {id: 1, img: '/img/1.png', stat: ""},
+function Cards(){
+    const [items, setItems] = useState([
+        {id: 1, img: '/img/1.jpg', stat: ""},
+        {id: 1, img: '/img/1.jpg', stat: ""},
         {id: 2, img: '/img/2.jpg', stat: ""},
         {id: 2, img: '/img/2.jpg', stat: ""},
-        {id: 3, img: '/img/3.png', stat: ""},
-        {id: 3, img: '/img/3.png', stat: ""},
-        {id: 4, img: '/img/4.png', stat: ""},
-        {id: 4, img: '/img/4.png', stat: ""},
-        {id: 5, img: '/img/5.png', stat: ""},
-        {id: 5, img: '/img/5.png', stat: ""},
-        {id: 6, img: '/img/6.png', stat: ""},
-        {id: 6, img: '/img/6.png', stat: ""},
+        {id: 3, img: '/img/3.jpg', stat: ""},
+        {id: 3, img: '/img/3.jpg', stat: ""},
+        {id: 4, img: '/img/4.jpg', stat: ""},
+        {id: 4, img: '/img/4.jpg', stat: ""},
+        {id: 5, img: '/img/5.jpg', stat: ""},
+        {id: 5, img: '/img/5.jpg', stat: ""},
+        {id: 6, img: '/img/6.jpg', stat: ""},
+        {id: 6, img: '/img/6.jpg', stat: ""},
         {id: 7, img: '/img/7.jpg', stat: ""},
         {id: 7, img: '/img/7.jpg', stat: ""},
         {id: 8, img: '/img/8.jpg', stat: ""},
         {id: 8, img: '/img/8.jpg', stat: ""},
-        {id: 9, img: '/img/9.png', stat: ""},
-        {id: 9, img: '/img/9.png', stat: ""},
+        {id: 9, img: '/img/9.jpg', stat: ""},
+        {id: 9, img: '/img/9.jpg', stat: ""},
         {id: 10, img: '/img/10.jpg', stat: ""},
         {id: 10, img: '/img/10.jpg', stat: ""},
         {id: 11, img: '/img/11.jpg', stat: ""},
@@ -29,22 +29,72 @@ const Cards = () => {
         {id: 12, img: '/img/12.jpg', stat: ""},
         {id: 13, img: '/img/13.jpg', stat: ""},
         {id: 13, img: '/img/13.jpg', stat: ""},
-        {id: 14, img: '/img/14.png', stat: ""},
-        {id: 14, img: '/img/14.png', stat: ""}]
-        .sort(() => Math.random() - 0.5))
+        {id: 14, img: '/img/14.jpg', stat: ""},
+        {id: 14, img: '/img/14.jpg', stat: ""},
+        {id: 15, img: '/img/15.jpg', stat: ""},
+        {id: 15, img: '/img/15.jpg', stat: ""},
+        {id: 16, img: '/img/16.jpg', stat: ""},
+        {id: 16, img: '/img/16.jpg', stat: ""},
+        {id: 17, img: '/img/17.jpg', stat: ""},
+        {id: 17, img: '/img/17.jpg', stat: ""},
+        {id: 18, img: '/img/18.jpg', stat: ""},
+        {id: 18, img: '/img/18.jpg', stat: ""},
+        {id: 19, img: '/img/19.jpeg', stat: ""},
+        {id: 19, img: '/img/19.jpeg', stat: ""},
+        {id: 20, img: '/img/20.jpg', stat: ""},
+        {id: 20, img: '/img/20.jpg', stat: ""},
+        {id: 21, img: '/img/21.jpg', stat: ""},
+        {id: 21, img: '/img/21.jpg', stat: ""},
+        {id: 22, img: '/img/22.jpg', stat: ""},
+        {id: 22, img: '/img/22.jpg', stat: ""},
+        {id: 23, img: '/img/23.jpeg', stat: ""},
+        {id: 23, img: '/img/23.jpeg', stat: ""},
+        {id: 24, img: '/img/24.jpeg', stat: ""},
+        {id: 24, img: '/img/24.jpeg', stat: ""},
+        {id: 25, img: '/img/25.jpeg', stat: ""},
+        {id: 25, img: '/img/25.jpeg', stat: ""},
+        {id: 26, img: '/img/26.jpeg', stat: ""},
+        {id: 26, img: '/img/26.jpeg', stat: ""}]
+.sort(() => Math.random() - 0.5))
 
-        const [prev, setPrev] = useState(-1)
+    const [prev, setPrev] = useState(-1)
 
-        function handleClick(id){
-            alert(id)
+    function check(current){
+        if(items[current].id == items[prev].id){
+            items[current].stat = "correct"
+            items[prev].stat = "correct"
+            setItems([...items])
+            setPrev(-1)
+        }else{
+            items[current].stat = "wrong"
+            items[prev].stat = "wrong"
+            setItems([...items])
+            setTimeout(() => {
+                items[current].stat = ""
+                items[prev].stat = ""
+                setItems([...items])
+                setPrev(-1)
+            }, 1000)
         }
-    return ( 
-         <div className="mid">
-            {items.map((item, index) => (
-                <Card key = {index} item ={item} id={index} handleClick = {handleClick}/>
-            ))}
-         </div> 
-     );
+    }
+
+    function handleClick(id){
+        if(prev === -1){
+            items[id].stat = "active"
+            setItems([...items])
+            setPrev(id)
+        }else{
+            check(id)
+        }
+    }
+
+    return (
+        <div className="container">
+            { items.map((item, index) => (
+                <Card key={index} item={item} id={index} handleClick={handleClick} />
+            )) }
+        </div>
+    )
 }
- 
-export default Cards;
+
+export default Cards
